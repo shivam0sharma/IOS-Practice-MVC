712018d1996c508c2823ee3df45f0667160ecb18
Shaisan2 <shivam0sharma@icloud.com>
2017-05-26 12:06:15 -0400

    Created the Person Model Class.

--- MVC/Person.swift
+++ MVC/Person.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+class Person {
+    private var _firstName: String
+    private var _lastName: String
+    
+    var firstName: String {
+        return _firstName
+    }
+    
+    var lastName: String {
+        return _lastName
+    }
+    
+    var fullName: String {
+        return "\(_firstName) \(_lastName)"
+    }
+    
+    init(firstName: String, lastName: String) {
+        self._firstName = firstName
+        self._lastName = lastName
+    }
+}
